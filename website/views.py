a97de1358475ab814c2455610549b12c0ec4950a
se_home <94399429+seffa1@users.noreply.github.com>
2021-12-29 06:28:53 -0500

    "continued"

--- website/views.py
+++ website/views.py
@@ -1,9 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
+# When this function is called, it renders the html in "home.html"
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
